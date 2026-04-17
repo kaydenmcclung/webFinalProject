63d8a8723a3603902ff633df56fb2206c98e0fc5
kaydenmcclung <kaydenmcclung@gmail.com>
2026-04-17 15:34:40 -0500

    fixed sidebar
    sidebar wasn't appearing so I fixed it

--- webFinalProject/src/components/Sidebar.jsx
+++ webFinalProject/src/components/Sidebar.jsx
@@ -3,14 +3,16 @@ import '../App.css'
 import { Link } from "react-router-dom"
 
 const Sidebar = () => {
-  <nav style={{ width: '200px', height: '100vh', display: 'flex', flexDirection: 'column' }}>
-    <h2>Navigation</h2>
-    <Link to="/">Dashboard</Link>
-    <Link to="/addtransaction">Add Transaction</Link>
-    <Link to="/history">Transaction History</Link>
-    <Link to="/reports">Reports</Link>
-    <Link to="/insights">Insights</Link>
-  </nav>
+  return (
+    <nav style={{ width: '200px', height: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <h2>Navigation</h2>
+      <Link to="/">Dashboard</Link>
+      <Link to="/addtransaction">Add Transaction</Link>
+      <Link to="/history">Transaction History</Link>
+      <Link to="/reports">Reports</Link>
+      <Link to="/insights">Insights</Link>
+    </nav>
+  )
 };
 
 export default Sidebar
