b6ca8ccb892dda8621f17495ec4de40b8c08f5fa
kaydenmcclung <kaydenmcclung@gmail.com>
2026-04-17 20:41:30 -0500

    Update ChartCard.jsx

--- webFinalProject/src/components/ChartCard.jsx
+++ webFinalProject/src/components/ChartCard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import '../App.css'
 
-const ChartCard = ({type = "small", title = "Undefined Title"}) => {
+const ChartCard = ({type = "smallChart", title = "Undefined Title"}) => {
     return (
         <div className = {type}>
             <h2>{title}</h2>
